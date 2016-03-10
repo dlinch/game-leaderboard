e90d6d726dc24f1205fee835e2a8567452ecf00f
dlinch <dlinch33@gmail.com>
2016-03-10 11:49:04 -0700

    added dotenv module to read environment variables

--- knexfile.js
+++ knexfile.js
@@ -1,26 +1,25 @@
-// Update with your config settings.
+require('dotenv').load();
 
 module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/leaderboard'
+    connection: 'postgres://localhost/leaderboard',
   },
-
 
   production: {
    client: 'pg',
    connection: {
-     user: process.env.PG_USERNAME,
+     user: process.env.PG_USER,
      password: process.env.PG_PASSWORD,
      host: process.env.PG_HOST,
      port: process.env.PG_PORT,
      database: process.env.PG_DATABASE,
-     ssl: true
+     ssl: true,
    },
    pool: {
      min: 2,
-     max: 10
+     max: 20
    },
    migrations: {
      tableName: 'knex_migrations'
